a3fc4532d2c72d261d9f25e92cf80f3777e6ec77
Rob Weber <rob.weber87@gmail.com>
2020-02-08 13:48:59 -0500

    change config file

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 const aws = require('aws-sdk');
 
-const envConfirmCode = new aws.S3({
+const DB_URL = new aws.S3({
   uri: process.env.uri
 });
 
 module.exports = {
-  mongoURI: envConfirmCode.config.uri
+  mongoURI: DB_URL.config.uri
 };
